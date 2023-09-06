80e0942da3454ed8162db277a9b5756336262aa7
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-09-06 11:19:52 +0200

    job 04

---
+++ jour-01/job-04/index.php
@@ -0,0 +1,20 @@
+<?php
+
+function my_fizz_buzz(int $length) : array {
+    $fizzBuzzArray = [];
+    
+    for ($i = 1; $i - 1 < $length; $i++) {
+        echo($i);
+        if ($i % 3 === 0 && $i % 5 === 0) {
+            $fizzBuzzArray[] = 'FizzBuzz';
+        } elseif ($i % 3 === 0) {
+            $fizzBuzzArray[] = 'Fizz';
+        } elseif ($i % 5 === 0) {
+            $fizzBuzzArray[] = 'Buzz';
+        } else  {
+            $fizzBuzzArray[] = $i;
+        }
+    }
+
+    return $fizzBuzzArray;
+}
